7ffb93621b831e79a8b602b742b3297793cdfa1f
vanyatoneva <vanyattoneva@gmail.com>
2019-04-04 11:52:58 +0300

    verticalHistogram

---
+++ Project2/Project2/verticalHistogram.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+
+#define MAXW 100
+
+/*horisontal histogram , shows to 100 words(MAXW)*/
+
+main() {
+
+	int c;
+	int numOfWords = 0;
+	int lentghOfWord = 0;
+	int lengths[MAXW];
+	int lonW = 0;
+
+	for (int i = 0; i <= MAXW; ++i) {
+		lengths[i] = 0;
+	}
+
+	/*making and array with the lenght of words - IT'S OKAY*/
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\n' || c == '\t') {
+			lengths[numOfWords] = lentghOfWord;
+			++numOfWords;
+			lentghOfWord = 0;
+		}
+		else {
+			++lentghOfWord;
+		}
+		
+	}
+	
+	/*Checking the longest word*/
+	for (int i = 0; i <= numOfWords; i++) {
+		if (lonW < lengths[i]) {
+			lonW = lengths[i];
+		}
+	}
+
+	for (int i = lonW; i > 0; --i) {
+		for (int j = 0; j <= numOfWords; j++) {
+			if (lengths[j] >= i) {
+				printf("[]");
+			}
+			else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	
+	
+}
