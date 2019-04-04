9d927541e3dda4c45c131a4bdfa736d295b92147
vanyatoneva <vanyattoneva@gmail.com>
2019-04-04 11:05:07 +0300

    horisontal histogram

---
+++ Project2/Project2/myHistogram.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+
+#define MAXW 100
+
+/*horisontal histogram , shows to 100 words(MAXW)*/
+
+main() {
+
+	int c;
+	int numOfWords = 0;
+	int lentghOfWord = 0;
+	int lengths[MAXW];
+
+
+	for (int i = 0; i <= MAXW; ++i) {
+		lengths[i] = 0;
+	}
+
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ' || c == '\n' || c == '\t') {
+			lengths[numOfWords] = lentghOfWord;
+			++numOfWords;
+			lentghOfWord = 0;
+		}
+		else {
+			++lentghOfWord;
+		}
+
+	}
+
+	for (int i = 0; i <= numOfWords; ++i) {
+		for (int j = 0; j < lengths[i]; ++j) {
+			putchar('#');
+		}
+		putchar('\n');
+	}
+
+}
