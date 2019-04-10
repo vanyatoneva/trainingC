7c50ed03db4ff2d3d932cad90c66637ca597cca2
vanyatoneva <vanyattoneva@gmail.com>
2019-04-10 17:38:45 +0300

    Update 3-3expand.c
    fixed - now works with a-b-c case

--- Project3/Project3/3-3expand.c
+++ Project3/Project3/3-3expand.c
@@ -4,7 +4,7 @@
 void expand(void);
 
 main() {
-	char s[] = {'t', '0', '-', '5', 'a', '-', 'd', '-', 'A', 'Y', '-', 'A', '-', 'F', '-', '\0'};
+	char s[] = { 't', '0', '-', '5', 'a', '-', 'd', '-', 'k', 'A', 'Y', '-', 'A', '-', 'F', '-', '\0' };
 	char s2[1000];
 	expand(s, s2);
 	printf("%s", s2);
@@ -16,22 +16,22 @@ void expand(char s1[], char s2[]) {
 	int i = 0;
 	for (i = 0; i < strlen(s1) - 2; i++) {
 		if (s1[i] >= 48 && s1[i] <= 57 && s1[i + 1] == '-' && s1[i + 2] >= 48 && s1[i + 2] <= 57 && s1[i] < s1[i + 2]) {
-			for (int k = s1[i]; k <= s1[i+2]; k++) {
+			for (int k = s1[i]; k < s1[i + 2]; k++) {
 				s2[j++] = k;
 			}
-			i += 2;
+			i++;
 		}
-		else if (s1[i] >= 65 && s1[i] <= 90 && s1[i + 1] == '-' && s1[i + 2] >= 65 && s1[i + 2] <= 90 && s1[i] < s1[i+2]) {
-			for (int k = s1[i]; k <= s1[i + 2]; k++) {
+		else if (s1[i] >= 65 && s1[i] <= 90 && s1[i + 1] == '-' && s1[i + 2] >= 65 && s1[i + 2] <= 90 && s1[i] < s1[i + 2]) {
+			for (int k = s1[i]; k < s1[i + 2]; k++) {
 				s2[j++] = k;
 			}
-			i += 2;
+			i++;
 		}
 		else if (s1[i] >= 97 && s1[i] <= 122 && s1[i + 1] == '-' && s1[i + 2] >= 97 && s1[i + 2] <= 122 && s1[i] < s1[i + 2]) {
-			for (int k = s1[i]; k <= s1[i + 2]; k++) {
+			for (int k = s1[i]; k < s1[i + 2]; k++) {
 				s2[j++] = k;
 			}
-			i += 2;
+			i++;
 		}
 		else {
 			s2[j++] = s1[i];
@@ -39,5 +39,6 @@ void expand(char s1[], char s2[]) {
 	}
 	s2[j] = s1[i];
 	s2[j + 1] = s1[i + 1];
-	
+	s2[j + 2] = s1[i + 2];
+
 }
