0626073b37f34f95655cb0d2783bdc4c59d00532
vanyatoneva <vanyattoneva@gmail.com>
2019-04-12 17:49:21 +0300

    Update rectangles.c

--- Project3/Project3/rectangles.c
+++ Project3/Project3/rectangles.c
@@ -21,13 +21,13 @@ int doOverlap(void);
 main() {
 	int arr[] = { 5, 3, 2, 1 };
 	Rectangle rec1, rec2;
-	rec1.leftX = 1;
-	rec1.bottomY = 1;
-	rec1.width = 4;
+	rec1.leftX = -1;
+	rec1.bottomY = -3;
+	rec1.width = -4;
 	rec1.height = 4;
 
-	rec2.leftX = 1;
-	rec2.bottomY = 1;
+	rec2.leftX = 2;
+	rec2.bottomY = -1;
 	rec2.width = 5;
 	rec2.height = 5;
 
@@ -37,7 +37,7 @@ main() {
 
 void match(Rectangle r1, Rectangle r2) {
 
-	if (doOverlap(r1, r2)) {
+	if (doOverlap(r1, r2) == 1) {
 		if (r1.leftX == r2.leftX && r1.bottomY == r2.bottomY && r1.height == r2.height && r1.width == r2.width) {
 			printf("The rectangles are the same!\n");
 			return;
@@ -51,20 +51,31 @@ void match(Rectangle r1, Rectangle r2) {
 		ul.y = (r1.bottomY + r1.height) < (r2.bottomY + r2.height) ? (r1.bottomY + r1.height) : (r2.bottomY + r2.height);
 		ur.x = dr.x;
 		ur.y = ul.y;
+		int area = (ur.x - ul.x) * (ur.y - dr.y);
+		if (area == 0) {
+			printf("They don't overlap, but touch sides");
+			return;
+		}
 		printf("Down left x = %d, y = %d\n", dl.x, dl.y);
 		printf("Down right x = %d, y = %d\n", dr.x, dr.y);
 		printf("Up lef x = %d, y = %d\n", ul.x, ul.y);
 		printf("Up right x = %d, y = %d\n", ur.x, ur.y);
 		printf("The area is : %d\n", (ur.x - ul.x) * (ur.y - dr.y));
 	}
+	else if (doOverlap(r1, r2) == -1) {
+		printf("Width or height of rectangle can not be negative num!");
+	}
 	else {
 		printf("They don't overlap");
 	}
-	
+
 }
 
 
 int doOverlap(Rectangle rec1, Rectangle rec2) {
+	if (rec1.height < 0 || rec1.width < 0 || rec2.height < 0 || rec2.width < 0) {
+		return -1;
+	}
 	if ((rec1.leftX < rec2.leftX && (rec1.leftX + rec1.width) < rec2.leftX) || (rec1.leftX > (rec2.leftX + rec2.width) && (rec1.leftX + rec1.width) > (rec2.leftX + rec2.width))) {
 		return 0;
 	}
@@ -74,7 +85,7 @@ int doOverlap(Rectangle rec1, Rectangle rec2) {
 	else {
 		return 1;
 	}
-	
+
 }
 
 
