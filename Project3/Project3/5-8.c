b555b85a1f4ecdb0669d4837523bbd67086ab62d
vanyatoneva <vanyattoneva@gmail.com>
2019-04-15 15:42:14 +0300

    Create 5-8.c

---
+++ Project3/Project3/5-8.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+
+static char daytab[2][13] = {
+{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
+{0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+};
+
+int day_of_year(int year, int month, int day);
+void month_day(int year, int yearday, int* pmonth, int* pday);
+
+
+
+main() {
+	int* m;
+	int* d;
+	printf("%d", day_of_year(1988, 2, 30));
+	month_day(1988, 368, &m, &d);
+	printf("\n %d, %d", d, m);
+}
+
+int day_of_year(int year, int month, int day) { /*set day of year from month and day*/
+	int i, leap;
+	if (month > 12) {
+		printf("The year has only 12 months! Yo've entered ");
+		return month;
+	}
+	leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	if (day > daytab[leap][month]) {
+		printf("The days you entred are more than has in this month! You've entered ");
+		return day;
+	}
+	else {
+		for (i = 1; i < month; i++) {
+			day += daytab[leap][i];
+		}
+		return day;
+	}
+	
+}
+
+void month_day(int year, int yearday, int* pmonth, int* pday) {
+	int leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+	if (yearday > 365 + leap) {
+		printf("This year has only %d days!", (365 + leap));
+		return;
+	}
+	int i;
+	for (i = 1; yearday > daytab[leap][i]; i++) {
+		yearday -= daytab[leap][i];
+	}
+	*pmonth = i;
+	*pday = yearday;
+}
